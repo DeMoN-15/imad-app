c117659347f6e74aea80e14c4e901565ac3d7c74
DeMoN-15 <renjeesh.n15@gmail.com>
2018-02-22 00:16:22 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-app.get('onething', function (req, res) {
+app.get('/onething', function (req, res) {
   res.send("hi this is not valid");
 });
 
